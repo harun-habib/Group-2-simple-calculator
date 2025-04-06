dcdd1ef4b841072de54423f3cf1d060dff837ad7
habte127 <abelayele127@gmail.com>
2025-04-06 22:59:40 +0300

    create addition

--- calculator.cpp
+++ calculator.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 int substruct(int a, int b);
 int division(int a, int b);
+int addition(int a, int b);
+
 
 int main() {
 int num1,num2;
 cout<<"result"<<substruct(num1,num2)<<endl;
 cout<<"result"<<division(num1,num2)<<endl;
+cout<<"result"<<addition(num1,num2)<<endl;
+
 
 
     return 0;
@@ -14,6 +18,11 @@ cout<<"result"<<division(num1,num2)<<endl;
 int substruct(int a, int b) {
     return a-b;
 }
+
 int division(int a, int b) {
     return a/b;
+}
+
+int addition(int a, int b) {
+    return a+b;
 }
