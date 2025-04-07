38431a0cc6974e7c3d97b086f54fa61ee84b0d06
abey94689 <abinet414@gmail.com>
2025-04-08 00:57:03 +0300

    square root

--- calculator.cpp
+++ calculator.cpp
@@ -5,6 +5,7 @@ int multiplication(int a, int b);
 int substruct(int a, int b);
 int division(int a, int b);
 int addition(int a, int b);
+int squareroot(int a, int b);
 
 
 
@@ -23,6 +24,7 @@ cout<<"Multiplication Result: "<<multiplication(num1,num2)<<endl;
 cout<<"Substruct Result: "<<substruct(num1,num2)<<endl;
 cout<<"Division Result:  "<<division(num1,num2)<<endl;
 cout<<"Addition Result: "<<addition(num1,num2)<<endl;
+cout<<"Squareroot Result:  "<<squareroot(num1,num2)<<endl;
 
 
     return 0;
@@ -42,6 +44,18 @@ int multiply(int a, int b){
     
     
     int addition(int a, int b) {
+
+
+
         return a+b;
     
     }
+
+    int squareroot(int a, int b) {
+        return sqrt(num1);
+    
+    } 
+
+
+
+
