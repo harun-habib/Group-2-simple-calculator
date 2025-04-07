0b70a897ae2ba970df8d135f6b1eb8fc0c47a2a3
fenu12 <fenut12@gmail.com>
2025-04-08 00:05:55 +0300

    implement addition

--- calculator.cpp
+++ calculator.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int multiplication(int a, int b);
 int substruct(int a, int b);
 int division(int a, int b);
+int addition(int a, int b);
+
+
 
 
 int main() {
@@ -15,10 +18,11 @@ cout<<"Enter The Second Number : ";
 cin>>num2;
 cout<<endl;
 cout<<"The Results "<<endl;
+
 cout<<"Multiplication Result: "<<multiplication(num1,num2)<<endl;
 cout<<"Substruct Result: "<<substruct(num1,num2)<<endl;
 cout<<"Division Result:  "<<division(num1,num2)<<endl;
-
+cout<<"Addition Result: "<<addition(num1,num2)<<endl;
 
 
     return 0;
@@ -31,5 +35,13 @@ int substruct(int a, int b) {
 int division(int a, int b) {
     return a/b;
 }
-int multiply(int a, int b) { return a * b; }
-
+int multiply(int a, int b){
+    
+    return a * b; }
+    
+    
+    
+    int addition(int a, int b) {
+        return a+b;
+    
+    }
