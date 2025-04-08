cedbbd6ee7c4a0ce4d76a8b7e6cdf116cafcc08f
harun habib <zulbijadinhabibharun3755@gmail.com>
2025-04-08 10:50:13 +0300

    simple calculator

--- calculator.cpp
+++ calculator.cpp
@@ -13,30 +13,45 @@ int reminder(int a, int b);
 
 
 int main() {
-int num1,num2;
-char op;
-cout<<"Enter The First Number : ";
-cin>>num1;
-cout<<"Enter The Second Number : ";
-cin>>num2;
-cout<<endl;
-cout<<"The Results "<<endl;
+    int num1, num2; 
+    char operation; 
+ 
+    cout << "Enter two numbers: "; 
+    cin >> num1 >> num2; 
+ 
+    cout << "Choose operation (+, -, *, /): "; 
+    cin >> operation; 
+ 
+    switch (operation) { 
+        case '*': 
 
 cout<<"Multiplication Result: "<<multiplication(num1,num2)<<endl;
+break; 
+case '-': 
 
 cout<<"Substruct Result: "<<substruct(num1,num2)<<endl;
-
+break; 
+case '/': 
 
 cout<<"Division Result:  "<<division(num1,num2)<<endl;
 
+break; 
+case '+': 
 cout<<"Addition Result: "<<addition(num1,num2)<<endl;
 
+break; 
+case 'root': 
 
 cout<<"Squareroot Result:  "<<squareroot(num1,num2)<<endl;
 
+break; 
+case '%': 
 cout<<"reminder Result: "<<reminder(num1,num2)<<endl;
 
-
+break; 
+default: 
+    cout << "Invalid operation!" << endl; 
+}
 
 
     return 0;
@@ -56,8 +71,6 @@ int multiply(int a, int b){
     
     
     int addition(int a, int b) {
-
-
 
         return a+b;
     
